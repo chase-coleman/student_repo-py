9bfcdf166072c80bdce09ec9b6983d1620009c95
YOUR_NAME <YOUR_EMAIL>
2025-02-10 19:46:56 -0600

    Completed.

--- student_registry.py
+++ student_registry.py
@@ -1,7 +1,7 @@
 # import re
 grade_list = ['9th', '10th', '11th', '12th']
 class Student:
-    def __init__(self, name, age=12, grade="12th"):
+    def __init__(self, name, age=13, grade="12th"):
         self._name = name
         self._age = age
         self._grade = grade
@@ -13,29 +13,30 @@ class Student:
     def name(self):
         return self._name
     @name.setter
-    def set_name(self, new_name):
-        if isinstance(new_name, str) and len(new_name) > 3:
-            self._name = new_name
+    def name(self, new_name):
+        if isinstance(new_name, str) and len(new_name) >= 3:
+            self._name = new_name.title()
         else:
             print("Invalid name format.") 
     @property
     def age(self):
         return self._age
     @age.setter
-    def set_age(self, new_age):
+    def age(self, new_age):
         if isinstance(new_age, int) and (new_age in range(12, 18)):
             self._age = new_age
         else:
             print("invalid age value.")
     @property
-    def get_grade(self):
-        return self.grade
-    @get_grade.setter
-    def set_grade(self, new_grade):
+    def grade(self):
+        return self._grade
+    @grade.setter
+    def grade(self, new_grade):
         # bool(re.match(r"^(9th|10th|11th|12th)$", stu_grade)) == True
         if isinstance(new_grade, str) and new_grade in grade_list:
             self._grade = new_grade
-    
+        else: print("Invalid grade value.")
+
     def advance(self):
         current_grade_index = grade_list.index(self.get_grade)
         if current_grade_index == 3:
